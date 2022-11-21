197a2fdf8b3d1e00b00271debb3942e405caee0e
Adriel Mendes <adriel.mendes@checklistfacil.com>
2022-11-21 02:27:58 -0300

    fix: utilize functions on home page componente too

--- apps/app/src/app/pages/home/home-page.component.ts
+++ apps/app/src/app/pages/home/home-page.component.ts
@@ -1,4 +1,6 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { getRestAndSumAndDivide } from '@function-vs-service/shared/interfaces/util-explore';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+import { getDivide, getRest, getSum } from '@function-vs-service/shared/interfaces/util-discovery';
 
 @Component({
   selector: 'function-vs-service-home-page',
@@ -6,4 +8,14 @@ import { Component, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./home-page.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class HomePageComponent {}
+export class HomePageComponent implements OnInit {
+  value1 = 10;
+  value2 = 20;
+
+  ngOnInit() {
+    console.log(getRest(this.value1, this.value2));
+    console.log(getSum(this.value1, this.value2));
+    console.log(getDivide(this.value1, this.value2));
+    console.log(getRestAndSumAndDivide(this.value1, this.value2));
+  }
+}
